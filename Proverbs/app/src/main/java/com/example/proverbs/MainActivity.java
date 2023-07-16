f0a7951fd2dcaccafb7e135d617cc0b91d26164b
Timothy Moniaga <tim.moniaga@gmail.com>
2023-07-16 14:35:23 +1000

    completed app

--- Proverbs/app/src/main/java/com/example/proverbs/MainActivity.java
+++ Proverbs/app/src/main/java/com/example/proverbs/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.proverbs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.telecom.Call;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,8 +30,11 @@ public class MainActivity extends AppCompatActivity {
     EditText edtPrompt;
     Button btnTranslate;
 
-    OkHttpClient client;
+    private ProgressBar progressBar;
+    private ConstraintLayout rootLayout;
 
+
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,40 +44,54 @@ public class MainActivity extends AppCompatActivity {
         txtResponse = findViewById(R.id.textResponse);
         edtPrompt = findViewById(R.id.editTextPrompt);
         btnTranslate = findViewById(R.id.buttonTranslate);
+        progressBar = findViewById(R.id.progressBar);
+        rootLayout = findViewById(R.id.rootLayout);
 
-        client = new OkHttpClient();
+
     }
-
-
-
 
     public void generatePrompt(View view) {
 
-        String sentence = edtPrompt.getText().toString();
-        String prompt = "From now on you are a native chinese speaker attempting to relay what I say into some proverb.  I only want the english translation in the response with a brief meaning after. To start I want you to turn this int a proverb: [" + sentence + "]";
+        btnTranslate.setEnabled(false);
+        progressBar.setVisibility(View.VISIBLE);
 
+        String sentence = edtPrompt.getText().toString();
+        if (sentence.length() <= 0 ) {
+            sentence = "I am blank";
+        }
+
+        String prompt = "From now on you are a native chinese speaker attempting to relay what I say into some proverb.  I only want the english translation in the response with a brief meaning after. I want the response to be formatted as such: \"English translation\" - \" Chinese proverb\" - (Chinese pronunciation) \n\ngit s Meaning: Explanation of proverb. To start I want you to turn this into a proverb: [" + sentence + "]";
 
         MediaType mediaType = MediaType.parse("application/json");
 
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("model","gpt-3.5-turbo");
-            jsonObject.put("prompt", prompt);
-            jsonObject.put("max_tokens", 64);
+            jsonObject.put("model", "gpt-3.5-turbo");
+            JSONArray messagesArray = new JSONArray();
+            JSONObject messageObject = new JSONObject();
+            messageObject.put("role", "system");
+            messageObject.put("content", prompt);
+            messagesArray.put(messageObject);
+            JSONObject userInputObject = new JSONObject();
+            userInputObject.put("role", "user");
+            userInputObject.put("content", sentence);
+            messagesArray.put(userInputObject);
+            jsonObject.put("messages", messagesArray);
+            jsonObject.put("max_tokens", 128);
             jsonObject.put("temperature", 0.7);
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
 
-
         RequestBody body = RequestBody.create(mediaType, jsonObject.toString());
 
         Request request = new Request.Builder()
-                .url("https://api.openai.com/v1/completions")
-                .addHeader("Authorization", Keys.OPEN_AI)
+                .url("https://api.openai.com/v1/chat/completions")
+                .addHeader("Authorization", "Bearer " + Keys.OPEN_AI)
                 .addHeader("Content-Type", "application/json")
                 .post(body)
                 .build();
+
 
         client.newCall(request).enqueue(new Callback() {
             @Override
@@ -88,34 +107,32 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject json = new JSONObject(responseData);
                         JSONArray choices = json.getJSONArray("choices");
                         JSONObject choice = choices.getJSONObject(0);
-                        String englishTranslation = choice.getString("text");
-
-                        // Process the English translation and provide a brief meaning
-                        String meaning = getMeaning(englishTranslation);
-
-                        // Update the UI with the translated text and its meaning
+                        String content = choice.getJSONObject("message").getString("content");
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                txtResponse.setText(meaning);
-                                // Update UI elements with the translated text and its meaning
+                                txtResponse.setText(content);
+                                progressBar.setVisibility(View.GONE);
+                                btnTranslate.setEnabled(true);
+
                             }
                         });
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 } else {
-                    txtResponse.setText("Error");
+                    final String errorMessage = "Error: " + response.message();
+                    System.out.println(response);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtResponse.setText(errorMessage);
+                            progressBar.setVisibility(View.GONE);
+                            btnTranslate.setEnabled(true);
+                        }
+                    });
                 }
             }
         });
     }
-
-    private String getMeaning(String translation) {
-        // Implement logic to retrieve the meaning of the proverb
-        // You can use a dictionary or other sources to look up the meaning based on the translated proverb
-        return "Brief meaning of the proverb";
-    }
-
-
 }
